4ac0698becb661429dc8dc6bdeace0c615c28d31
LiaisanPerfilova <perliaisan@gmail.com>
2023-04-24 15:09:34 -0400

    Final version

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redCircle: UIStackView!
+    @IBOutlet var yellowCircle: UIStackView!
+    @IBOutlet var greenCircle: UIView!
+    @IBOutlet var redButton: UIButton!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redButton.layer.cornerRadius = 20
+        redCircle.layer.cornerRadius = 75
     }
-
-
+    
+    @IBAction func buttonTapped() {
+        buttonTapped.setTitle("Next", for: .normal)
+    }
+    
 }
 
